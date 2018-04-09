8e37edd4332688021dbc8fc1fb5a106438306886
LiviaFerreira <Liviaararaquara@gmail.com>
2018-04-08 21:39:33 -0300

    V1- Cabeçalho do trabalho

---
+++ cabecalho.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Avaliação Dupla</title>
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+	</head>
+	<body>
+		<h1>T1</h1>
+		<nav>
+			<a href='index.php'>Início</a>
+			<a href='form_estado.php'>Cadastro Estado</a>
+			<a href='form_cidade.php'>Cadastro Cidade</a>
+			<a href='form_cadastro.php'>Cadastro Cliente</a>
+		</nav>
+	</body>
+</html>
